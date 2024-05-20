abc7cd1e3d1db3a727c393e30d0353266042c786
Tatyana Volkova <kameneva.tania@mail.ru>
2024-05-20 16:25:25 +0300

    adds props 'size' for title.jsx

--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -1,10 +1,16 @@
 import styles from "./style.module.css";
+import PropTypes from 'prop-types';
 
 /* компонент должен возвращать все предусмотренные макетом заголовки,
 от самого большого до самого маленького, т.е. используем props и children, а пока макет не утвержден, Title возвращает h1 */
 
-function Title({ text }) {
-  return <h1 className={styles.h1}>{text}</h1>;
+function Title({ text, size }) {
+  return <h1 className={`${styles.title} ${size ? `title_${size}` : ''}`}>{text}</h1>;
+}
+
+Title.propTypes = {
+  children: PropTypes.string,
+  size: PropTypes.string
 }
 
 export default Title;
