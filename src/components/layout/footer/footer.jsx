8524a005a81671c3b5107e4ea784cccf4ed2bb50
TanyaStone <kameneva.tania@mail.ru>
2024-05-22 18:29:10 +0300

    returns footer

---
+++ src/components/layout/footer/footer.jsx
@@ -0,0 +1,7 @@
+// import styles from './style.module.css';
+
+function Footer() {
+  return <></>;
+}
+
+export default Footer;
--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -1,14 +1,14 @@
 import Header from '../header/header';
 import MainPage from '../../pages/main-page/main-page';
+import Footer from '../footer/footer';
 import styles from './style.module.css';
 
 function PageWrapper() {
   return (
     <>
-    <div className='container'>
       <Header />
       <MainPage />
-      </div>
+      <Footer />
     </>
   );
 }
