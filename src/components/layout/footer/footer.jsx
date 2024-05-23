d7767303984b92f9be18e47530d032821459b252
Margarita Shishkova <margolen239@gmail.com>
2024-05-23 20:07:23 +0100

    Create the footer

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,7 +1,32 @@
-// import styles from './style.module.css';
+import Container from '../../ui/container/container';
+import Logo from '../../ui/logo/logo';
+
+import Telegram from '../../../assets/img/telegram.svg';
+import { TelegramLink } from '../../../utils/consts';
+
+import styles from './style.module.css';
 
 function Footer() {
-  return <></>;
+  return (
+    <footer className={styles.footer}>
+      <Container>
+        <div className={styles.footer__wrapper}>
+          <a href={TelegramLink}>
+            <img className={styles.footer__icon} src={Telegram} alt="" />
+          </a>
+          <a href="/">
+            <Logo className={styles.footer__logo} />
+          </a>
+          <p>
+            Сайт разработан{' '}
+            <a className={styles.footer__createdby} href="https://itgirlschool.com/">
+              ITGirls
+            </a>
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/ui/logo/logo.jsx
+++ src/components/ui/logo/logo.jsx
@@ -1,11 +1,13 @@
 // import {ReactComponent as LogoImage} from '/src/assets/logo.svg';
 /* закомментирован пока нерабочий код с Лого */
 
-function Logo() {
+function Logo({ className }) {
   return (
     <>
-    {/*  <LogoImage/> */}
-      <a href="#" target="blank">@pavlyuts_pro</a>
+      {/*  <LogoImage/> */}
+      <a href="#" target="blank" className={className}>
+        @pavlyuts_pro
+      </a>
     </>
   );
 }
