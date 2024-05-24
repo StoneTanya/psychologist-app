eca16c0e40c6aced32ee46b640b67cfa930d9057
Margarita Shishkova <91085065+Margolen@users.noreply.github.com>
2024-05-24 21:47:11 +0100

    Create work method block (#9)

---
+++ src/components/blocks/work-method/work-method.jsx
@@ -0,0 +1,35 @@
+import Title from '../../ui/title/title';
+import Container from '../../ui/container/container';
+import { TitleLevel, TitleSize } from '../../../utils/consts';
+import methodPhoto from '../../../assets/img/method-photo.png';
+
+import styles from './style.module.css';
+
+export default function WorkMethod() {
+  return (
+    <section>
+      <Container>
+        <div className={styles.method__wrapper}>
+          <div className={styles.method__img}>
+            <img src={methodPhoto} alt="Фотография психолога Яны Павлюц" />
+          </div>
+          <div className={styles.method__content}>
+            <p>Работаю в полимодальном подходе.</p>
+            <p>
+              Это значит, что в процессе консультации использую разные методы терапии (аналитическая
+              терапия, ДПДГ, эмоционально-образная психотерапия, элементы когнитивно-поведенческой
+              терапии) в зависимости от того, что лучше всего подходит для вашего запроса.
+            </p>
+            <Title
+              className={styles.method__highlight}
+              level={TitleLevel.H2}
+              size={TitleSize.SMALL}
+            >
+              Оказываю психологическое сопровождение и терапию людям, пережившим сексуальное насилие
+            </Title>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
--- src/components/ui/title/title.jsx
+++ src/components/ui/title/title.jsx
@@ -3,11 +3,11 @@ import styles from './style.module.css';
 /* компонент должен возвращать все предусмотренные макетом заголовки,
 от самого большого до самого маленького, т.е. используем props и children, а пока макет не утвержден, Title возвращает h1 */
 
-function Title({ children, size, level }) {
+function Title({ children, size, level, className }) {
   const CustomTitleTag = `h${level}`;
 
   return (
-    <CustomTitleTag className={`${styles.title} ${size ? `title_${size}` : ''}`}>
+    <CustomTitleTag className={`${styles.title} ${size ? `title_${size}` : ''} ${className || ''}`}>
       {children}
     </CustomTitleTag>
   );
