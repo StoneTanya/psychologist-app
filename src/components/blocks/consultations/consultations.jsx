f9ad8f12244f4c0b0a78d5b1baf33c124b236b6d
Polina Makarova <sunnyfur9@gmail.com>
2024-05-25 20:43:14 +0500

    feat: consult block

--- src/components/blocks/consultations/consultations.jsx
+++ src/components/blocks/consultations/consultations.jsx
@@ -1,14 +1,47 @@
 import Title from '../../ui/title/title';
 import Container from '../../ui/container/container';
 import { TitleLevel } from '../../../utils/consts';
+import dataList from './data';
 
-// import styles from './style.module.css';
+import styles from './style.module.css';
 
 function Consultations() {
+  const lists = dataList();
+
   return (
-    <section className='wrapper'>
+    <section className={styles.consult}>
       <Container>
-        <Title level={TitleLevel.H2}>С какими запросами я могу помочь:</Title>
+        <Title level={TitleLevel.H2} className={styles.title}>
+          С какими запросами я могу помочь:
+        </Title>
+
+        <div className={styles.blocks}>
+          {lists.canHelp.map((block, index) => (
+            <div key={index} className={`${styles.block} ${styles.block_fill}`}>
+              {block.map((item, id) => (
+                <li key={id} className={styles.item}>
+                  {item}
+                </li>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        <Title level={TitleLevel.H3} className={styles.subtitle}>
+          С какими запросами не работаю:
+        </Title>
+
+        <div className={styles.blocks}>
+          {lists.notWork.map((block, index) => (
+            <div key={index} className={`${styles.block} ${styles.block_outlined}`}>
+              {block.map((item, id) => (
+                <li key={id} className={styles.item}>
+                  {item}
+                </li>
+              ))}
+            </div>
+          ))}
+        </div>
       </Container>
     </section>
   );
