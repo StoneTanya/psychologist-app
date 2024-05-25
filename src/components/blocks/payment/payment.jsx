8d7f88edddfaaf285bae90abd2ebcb6e9bb1f699
Tatyana Volkova <kameneva.tania@mail.ru>
2024-05-25 19:58:24 +0300

    adds media/adaptive for title, text, buttons and links (#16)

--- src/components/blocks/payment/payment.jsx
+++ src/components/blocks/payment/payment.jsx
@@ -2,14 +2,14 @@ import Button from '../../ui/button/button';
 import Title from '../../ui/title/title';
 import Container from '../../ui/container/container';
 import { TitleLevel, TelegramLink } from '../../../utils/consts';
-// import styles from './style.module.css';
+import styles from './style.module.css';
 
 function Payment() {
   return (
     <section className='wrapper'>
       <Container>
         <Title level={TitleLevel.H2}>Стоимость и оплата</Title>
-        <Button link={TelegramLink}>Записаться на консультацию</Button>
+        <Button link={TelegramLink} className={styles.payment__button}>Записаться на консультацию</Button>
       </Container>
     </section>
   );
--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -1,6 +1,4 @@
 import Container from '../../ui/container/container';
-import Logo from '../../ui/logo/logo';
-
 import Telegram from '../../../assets/img/telegram.svg';
 import { TelegramLink } from '../../../utils/consts';
 
@@ -11,12 +9,11 @@ function Footer() {
     <footer className={styles.footer}>
       <Container>
         <div className={styles.footer__wrapper}>
-          <a href={TelegramLink}>
+          <a href={TelegramLink} target='blank' className={styles.footer__link}>
             <img className={styles.footer__icon} src={Telegram} alt="" />
+            <p>@pavlyuts_pro</p>
           </a>
-          <a href="/">
-            <Logo className={styles.footer__logo} />
-          </a>
+
           <p className={styles.footer__copyright}>
             <span>&copy; </span> Created by{' '}
             <a className={styles.footer__createdby} href="https://itgirlschool.com/">
