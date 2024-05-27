7e64092c8c6acd83926afb53e8d51f53e18348c1
Margarita Shishkova <91085065+Margolen@users.noreply.github.com>
2024-05-27 13:48:11 +0100

    Adaptive for user-form block (#22)
    * Adaptive for user-form block
    
    * Change em to rem and remove redundant styles

--- src/components/blocks/user-form/user-form.jsx
+++ src/components/blocks/user-form/user-form.jsx
@@ -2,33 +2,31 @@ import Button from '../../ui/button/button';
 import Title from '../../ui/title/title';
 import Container from '../../ui/container/container';
 import { TitleLevel, FormLink } from '../../../utils/consts';
-import userFormPhoto from '../../../assets/img/userForm-photo.png';
 import styles from './style.module.css';
+
+import UserFormPhoto from '../../../assets/img/userForm-photo.png';
 
 function UserForm() {
   return (
     <section>
-      <Container>
-        <div className={styles.form__wrapper}>
+      <Container className={styles.container}>
+        <div className={styles.form__section}>
           <div className={styles.form__content}>
             <Title level={TitleLevel.H2}>Вопрос-ответ</Title>
-
             <p>
-              Здесь вы можете задать свой вопрос.
-              <br /> Форма анонимная, но для ответа вам нужно будет указать почту, на которую вы
-              ожидаете ответ.
+              Здесь вы можете задать свой вопрос. Форма анонимная, но для ответа вам нужно будет
+              указать почту, на которую вы ожидаете ответ.
             </p>
             <p>
               Некоторые ответы я могу публиковать на сайте. Вопрос остается анонимным: без контактов
               и любой другой персональной информации.
             </p>
-
             <Button link={FormLink} className={styles.form__link}>
               Задать вопрос
             </Button>
           </div>
           <div className={styles.form__img}>
-            <img src={userFormPhoto} alt="Фотография психолога Яны Павлюц" />
+            <img src={UserFormPhoto} alt="Фотография психолога Яны Павлюц" />
           </div>
         </div>
       </Container>
