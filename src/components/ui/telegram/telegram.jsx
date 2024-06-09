224d1155ab458d66a86207f4e43fa44542c4a762
Tatyana Volkova <kameneva.tania@mail.ru>
2024-06-09 18:19:03 +0300

    Update telegram.jsx
    removes div-wrapper

--- src/components/ui/telegram/telegram.jsx
+++ src/components/ui/telegram/telegram.jsx
@@ -2,14 +2,12 @@ import styles from './style.module.css';
 import TelegramIcon from '../../../assets/img/telegram.svg';
 import { TelegramLink } from '../../../utils/consts';
 
-function Telegram({ className }) {
+function Telegram() {
   return (
-    <div className={`${styles.telegram__wrapper} ${className}`}>
       <a href={TelegramLink} target="_blank" className={styles.telegram__link}>
         <img className={styles.telegram__icon} src={TelegramIcon} alt="Telegram icon" />
-        <p>@pavlyuts_pro</p>
+        @pavlyuts_pro
       </a>
-    </div>
   );
 }
 
