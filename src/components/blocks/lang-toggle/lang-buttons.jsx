afa165365ca9ea81926ef6b07c6b5423b27f8fcf
Tatyana Volkova <kameneva.tania@mail.ru>
2024-06-13 17:55:10 +0300

    adds handlers to language buttons and stylizes

--- src/components/blocks/lang-toggle/lang-buttons.jsx
+++ src/components/blocks/lang-toggle/lang-buttons.jsx
@@ -1,18 +1,48 @@
 import Button from '../../ui/button/button';
 import styles from './style.module.css';
 import i18n from '../../../i18n';
+import { useState } from 'react';
 
 function LangButtons({ className = '' }) {
+  const [isActiveButtonRU, setIsActiveButtonRU] = useState(true);
+  const [isActiveButtonEN, setIsActiveButtonEN] = useState(false);
+
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
 
+  const selectRUButton = () => {
+    setIsActiveButtonRU(!isActiveButtonRU);
+    setIsActiveButtonEN(!isActiveButtonEN);
+  };
+
+  const selectENButton = () => {
+    setIsActiveButtonEN(!isActiveButtonEN);
+    setIsActiveButtonRU(!isActiveButtonRU);
+  };
+
+  const handleClickRU = () => {
+    selectRUButton();
+    changeLanguage('ru');
+  };
+
+  const handleClickEN = () => {
+    selectENButton();
+    changeLanguage('en');
+  };
+
   return (
     <div className={className}>
-      <Button handler={() => changeLanguage('ru')} className={styles.button__lang}>
+      <Button
+        handler={() => handleClickRU()}
+        className={`${styles.button__lang} ${isActiveButtonRU ? styles.button__active : ''}`}
+      >
         RU
       </Button>
-      <Button handler={() => changeLanguage('en')} className={styles.button__lang}>
+      <Button
+        handler={() => handleClickEN()}
+        className={`${styles.button__lang} ${isActiveButtonEN ? styles.button__active : ''}`}
+      >
         EN
       </Button>
     </div>
