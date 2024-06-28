ff78697dd0cf22e53820a3f943585305ef7be9e6
Margarita Shishkova <margolen239@gmail.com>
2024-06-28 22:15:30 +0100

    SEO optimization

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,23 @@
+import { Helmet } from 'react-helmet';
+import { useTranslation } from 'react-i18next';
 import PageWrapper from '../layout/page-wrapper/page-wrapper';
 
 function App() {
-  return <PageWrapper />;
+  const { t, i18n } = useTranslation();
+  const keywords = t('about.fact2List', { returnObjects: true });
+
+  return (
+    <>
+      <Helmet>
+        <html lang={i18n.language} />
+        <title>{t('intro.subtitle')}</title>
+        <meta name="description" content={t('about.fact1')} />
+        <meta name="keywords" content={keywords.join('')} />
+        <meta name="robots" content="index, follow" />
+      </Helmet>
+      <PageWrapper />
+    </>
+  );
 }
 
 export default App;
